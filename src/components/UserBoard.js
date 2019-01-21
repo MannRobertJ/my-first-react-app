31c96f0a3e9a24c85440a3b3dbdfc45cf82eff1b
MannRobertJ <mann.robertj@gmail.com>
2019-01-21 14:14:24 +0100

    what will this do

--- src/components/UserBoard.js
+++ src/components/UserBoard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class User extends Component {
+export default class UserBoard extends Component {
   append() {
       return this.props.users.reduce((string, user) => {
         return string + "<br>" + `User: ${user}`
