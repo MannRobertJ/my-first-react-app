df670b030f71bb3af7727fcd8eaa40062c104e07
MannRobertJ <mann.robertj@gmail.com>
2019-01-21 15:10:22 +0100

    what will this do

--- src/components/User.js
+++ src/components/User.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 
 export default class User extends Component {
+  here = true;
+
+  toggleStatus() {
+    this.here = !this.here;
+  }
+
   render() {
     return (
       <p>
         User: <strong>{this.props.name}</strong>
+        {this.props.name} is {this.state.active ? "on" : "off"}
+        <button onClick="this.toggleStatus()">Toggle</button>
       </p>
     );
   }
 }
-
-
-
