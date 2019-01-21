6de70889e13dfee9831732d1f9d6e4c32d566152
MannRobertJ <mann.robertj@gmail.com>
2019-01-21 13:21:07 +0100

    what will this do

--- src/components/UserBoard.js
+++ src/components/UserBoard.js
@@ -8,7 +8,6 @@ export default class UserBoard {
     }
     render() {
         const usersPara = "<p id=\"users\"> </p> "
-        function appendUsers()
         return <div>
             <h3>User Board</h3>
             <p id="users">{this.appendUsers()}</p>
