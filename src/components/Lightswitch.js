5a8edb2f9b89d87a98bd3d6c7840a53ec258e0e8
MannRobertJ <mann.robertj@gmail.com>
2019-01-21 11:38:29 +0100

    lightswitch

--- src/components/Lightswitch.js
+++ src/components/Lightswitch.js
@@ -1,15 +1,18 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export default class LightSwitch extends Component {
-  state = { active: true };
+  state = { active: true }
+
+  toggle = () => {
+    this.setState({
+      active: !this.state.active
+    })
+  }
 
   render() {
-    return (
-      <div>
-        <p>
-          The light switch is <b>{this.state.active ? "on" : "off"}</b>
-        </p>
-      </div>
-    );
+    return (<div>
+      <p>The light switch is <b>{ this.state.active ? 'on' : 'off' }</b></p>
+      <button onClick={this.toggle}>Toggle</button>
+    </div>)
   }
 }
